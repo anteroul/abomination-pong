56af10bd567c28a3c5f7c5a27d7aa8882727f7f3
Uljas Lindell <58420474+anteroul@users.noreply.github.com>
2022-10-07 17:21:20 +0300

    refactoring

--- Src/structs.h
+++ Src/structs.h
@@ -5,19 +5,19 @@
 #ifndef SIGMAENGINE_STRUCTS_H
 #define SIGMAENGINE_STRUCTS_H
 
-typedef struct {
+struct {
     SDL_Point pos;
     SDL_Rect r;
-} Racket;
+};
 
-typedef struct {
+struct {
     SDL_Point speed;
     SDL_Rect r;
-} Ball;
+};
 
-typedef struct {
+struct {
     SDL_Texture* sprite;
     SDL_Point pos;
-} PowerUp;
+};
 
 #endif //SIGMAENGINE_STRUCTS_H
